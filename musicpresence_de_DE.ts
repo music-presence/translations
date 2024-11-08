0bbecf9090ccf368d4c9dc208c36482dbc1069f4
Jonas van den Berg <jonas@vandenbe.com>
2024-11-08 15:26:15 +0100

    Initial translation strings

---
+++ musicpresence_de_DE.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../src/app/app.cpp" line="1146"/>
+        <source>Presence disabled</source>
+        <comment>checkbox text: presence is currently disabled</comment>
+        <translation type="unfinished">Status deaktiviert</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1149"/>
+        <source>Click to enable</source>
+        <comment>click the checkbox to enable the presence again</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1153"/>
+        <source>Presence is enabled</source>
+        <comment>checkbox text: the presence is enabled, but not active</comment>
+        <translation>Status aktiviert</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1158"/>
+        <source>Waiting for Discord</source>
+        <comment>status text: discord is not detected yet</comment>
+        <translation>Warte auf Discord</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1164"/>
+        <source>Waiting for media playback</source>
+        <comment>status text: no media is detected on the device</comment>
+        <translation>Warte auf Medienwiedergabe</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1168"/>
+        <source>Presence is active</source>
+        <comment>checkbox text: media is shared in the user&apos;s discord activity</comment>
+        <translation>Status ist aktiv</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1391"/>
+        <source>Donate</source>
+        <comment>button</comment>
+        <translation type="unfinished">Spenden</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1393"/>
+        <source>Support my work with a small donation</source>
+        <comment>donate button: hover text</comment>
+        <translation type="unfinished">Unterstütze meine Arbeit mit einer kleinen Spende</translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1400"/>
+        <source>Exit</source>
+        <comment>button: close the application</comment>
+        <translation type="unfinished">Beenden</translation>
+    </message>
+</context>
+</TS>
---
+++ musicpresence_uk_UA.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../src/app/app.cpp" line="1146"/>
+        <source>Presence disabled</source>
+        <comment>checkbox text: presence is currently disabled</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1149"/>
+        <source>Click to enable</source>
+        <comment>click the checkbox to enable the presence again</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1153"/>
+        <source>Presence is enabled</source>
+        <comment>checkbox text: the presence is enabled, but not active</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1158"/>
+        <source>Waiting for Discord</source>
+        <comment>status text: discord is not detected yet</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1164"/>
+        <source>Waiting for media playback</source>
+        <comment>status text: no media is detected on the device</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1168"/>
+        <source>Presence is active</source>
+        <comment>checkbox text: media is shared in the user&apos;s discord activity</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1391"/>
+        <source>Donate</source>
+        <comment>button</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1393"/>
+        <source>Support my work with a small donation</source>
+        <comment>donate button: hover text</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/app.cpp" line="1400"/>
+        <source>Exit</source>
+        <comment>button: close the application</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
