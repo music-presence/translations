28c84c95dc30e49cc110319ac25f2daab29c2f24
MoonManPL <Michal.haskiewicz974@gmail.com>
2024-11-09 00:10:56 +0000

    Translated using Weblate (Polish)
    Currently translated at 35.5% (37 of 104 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/pl/

--- musicpresence_pl_PL.ts
+++ musicpresence_pl_PL.ts
@@ -211,7 +211,7 @@
         <location filename="../src/app/app.cpp" line="2186"/>
         <source>Show for how long media is paused</source>
         <comment>appearance: whether to show a timer in the status that counts how long the media has been paused</comment>
-        <translation>Pokaż na jak długo multimedia jest wstrzymane</translation>
+        <translation>Pokaż jak długo media są wstrzymane</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2206"/>
