fa91a4332667d1fdd0fe222cb8ec5f5cc0c15c92
MoonManPL <Michal.haskiewicz974@gmail.com>
2024-11-11 11:02:07 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (120 of 120 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/pl/

--- musicpresence_pl.ts
+++ musicpresence_pl.ts
@@ -512,7 +512,7 @@
         <location filename="../src/app/app.cpp" line="3227"/>
         <source>Click to install version {version}</source>
         <comment>update button</comment>
-        <translation type="unfinished"></translation>
+        <translation>Kliknij, żeby zainstalować wersję {version}</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3648"/>
