99aabfaf6c0f6191a3bdbd1361e74004033002e8
moe <admin@moe.christmas>
2024-11-11 11:08:03 +0000

    Translated using Weblate (Afrikaans)
    Currently translated at 100.0% (120 of 120 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/af/

--- musicpresence_af.ts
+++ musicpresence_af.ts
@@ -512,7 +512,7 @@
         <location filename="../src/app/app.cpp" line="3227"/>
         <source>Click to install version {version}</source>
         <comment>update button</comment>
-        <translation type="unfinished"></translation>
+        <translation>Klik om weergawe {version} te installeer</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3648"/>
