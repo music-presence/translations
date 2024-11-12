c84cfe39342dca1820e3fc57c4d3a08cdc6f68b4
Ayovu <ayovu@proton.me>
2024-11-12 21:23:58 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (129 of 129 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/uk/

--- musicpresence_uk.ts
+++ musicpresence_uk.ts
@@ -7,19 +7,19 @@
         <location filename="../src/app/app.cpp" line="1118"/>
         <source>disabled</source>
         <comment>label next to a disabled media player</comment>
-        <translation>вимкнуто</translation>
+        <translation>вимкнутий</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="1125"/>
         <source>enabled</source>
         <comment>label next to an enabled media player</comment>
-        <translation>увімкнуто</translation>
+        <translation>увімкнутий</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="1130"/>
         <source>now active</source>
         <comment>label next to an active media player</comment>
-        <translation>зараз активний</translation>
+        <translation>зараз відображається</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="1212"/>
