19603da648c9ecdb76355c78bfb692ac017508b3
Jonas van den Berg <jonas@vandenbe.com>
2024-11-12 21:10:41 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (129 of 129 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/de/

--- musicpresence_de.ts
+++ musicpresence_de.ts
@@ -454,7 +454,7 @@
         <location filename="../src/app/app.cpp" line="4323"/>
         <source>About</source>
         <comment>about the app: you may use {app_name} to refer to the application</comment>
-        <translation type="unfinished">Über</translation>
+        <translation>Über</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2875"/>
