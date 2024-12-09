5d8608468f71bd356e3ca058a59581b1950dc392
Snyri <gionofb99@gmail.com>
2024-12-09 14:05:06 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (135 of 135 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/it/

--- musicpresence_it.ts
+++ musicpresence_it.ts
@@ -283,31 +283,31 @@
         <location filename="../src/app/app.cpp" line="2778"/>
         <source>Media player logo</source>
         <comment>Placeholder image: The logo of the media player</comment>
-        <translation type="unfinished"></translation>
+        <translation>Logo del lettore multimediale</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2780"/>
         <source>Music note</source>
         <comment>Placeholder image: A music note</comment>
-        <translation type="unfinished"></translation>
+        <translation>Nota musicale</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2782"/>
         <source>Playback state</source>
         <comment>Placeholder image: The playback state of the song shown as a playing or paused icon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Stato della riproduzione</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2785"/>
         <source>CD</source>
         <comment>Placeholder image: An image of a physical CD (Compact Disc)</comment>
-        <translation type="unfinished"></translation>
+        <translation>CD</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2788"/>
         <source>{app_name} logo</source>
         <comment>Placeholder image: The Music Presence logo</comment>
-        <translation type="unfinished"></translation>
+        <translation>Logo di {app_name}</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2821"/>
@@ -338,7 +338,7 @@
         <location filename="../src/app/app.cpp" line="2767"/>
         <source>Placeholder for missing cover images</source>
         <comment>setting for placeholder images when there is no cover image</comment>
-        <translation type="unfinished"></translation>
+        <translation>Alternative per immagini di copertina mancanti</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2828"/>
