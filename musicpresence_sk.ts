b8148a882ac0f122cfab5846a49ab4632473eb7f
cheezton <cheezton@users.noreply.translate.codeberg.org>
2024-12-10 11:32:03 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (135 of 135 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/sk/

--- musicpresence_sk.ts
+++ musicpresence_sk.ts
@@ -325,31 +325,31 @@
         <location filename="../src/app/app.cpp" line="2778"/>
         <source>Media player logo</source>
         <comment>Placeholder image: The logo of the media player</comment>
-        <translation type="unfinished"></translation>
+        <translation>Logo prehrávača médií</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2780"/>
         <source>Music note</source>
         <comment>Placeholder image: A music note</comment>
-        <translation type="unfinished"></translation>
+        <translation>Nota</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2782"/>
         <source>Playback state</source>
         <comment>Placeholder image: The playback state of the song shown as a playing or paused icon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Stav prehrávania</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2785"/>
         <source>CD</source>
         <comment>Placeholder image: An image of a physical CD (Compact Disc)</comment>
-        <translation type="unfinished"></translation>
+        <translation>CD</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2788"/>
         <source>{app_name} logo</source>
         <comment>Placeholder image: The Music Presence logo</comment>
-        <translation type="unfinished"></translation>
+        <translation>Logo {app_name}</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2821"/>
@@ -378,7 +378,7 @@
         <location filename="../src/app/app.cpp" line="2767"/>
         <source>Placeholder for missing cover images</source>
         <comment>setting for placeholder images when there is no cover image</comment>
-        <translation type="unfinished"></translation>
+        <translation>Náhradný obrázok pre chýbajúce obrázky obalov</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2872"/>
