5f621fb011139a8940969d37c5d2397abe509bb4
longnuub <lehoanglong.hvt@elc.vn>
2024-12-11 11:08:05 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (135 of 135 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/vi/

--- musicpresence_vi.ts
+++ musicpresence_vi.ts
@@ -325,31 +325,31 @@
         <location filename="../src/app/app.cpp" line="2778"/>
         <source>Media player logo</source>
         <comment>Placeholder image: The logo of the media player</comment>
-        <translation type="unfinished"></translation>
+        <translation>Logo của trình phát</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2780"/>
         <source>Music note</source>
         <comment>Placeholder image: A music note</comment>
-        <translation type="unfinished"></translation>
+        <translation>Nốt nhạc</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2782"/>
         <source>Playback state</source>
         <comment>Placeholder image: The playback state of the song shown as a playing or paused icon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Trạng thái phát</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2785"/>
         <source>CD</source>
         <comment>Placeholder image: An image of a physical CD (Compact Disc)</comment>
-        <translation type="unfinished"></translation>
+        <translation>Đĩa CD</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2788"/>
         <source>{app_name} logo</source>
         <comment>Placeholder image: The Music Presence logo</comment>
-        <translation type="unfinished"></translation>
+        <translation>Logo của {app_name}</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2821"/>
@@ -378,7 +378,7 @@
         <location filename="../src/app/app.cpp" line="2767"/>
         <source>Placeholder for missing cover images</source>
         <comment>setting for placeholder images when there is no cover image</comment>
-        <translation type="unfinished"></translation>
+        <translation>Ảnh thế chỗ nếu không có ảnh bìa</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="2872"/>
