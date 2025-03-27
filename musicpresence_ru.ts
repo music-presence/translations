17f67b72118fc9c9649492f01b6d12a50a951824
Ayovu <ayovu@proton.me>
2025-03-27 11:34:17 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (137 of 137 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/ru/

--- musicpresence_ru.ts
+++ musicpresence_ru.ts
@@ -379,13 +379,13 @@
         <location filename="../src/app/app.cpp" line="3079"/>
         <source>Use the Spotify API, when listening to Spotify</source>
         <comment>general setting</comment>
-        <translation type="unfinished"></translation>
+        <translation>Использовать Spotify API, когда слушаешь Spotify</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3091"/>
         <source>Use the iTunes API, when listening to Apple Music</source>
         <comment>general setting</comment>
-        <translation type="unfinished"></translation>
+        <translation>Использовать iTunes API, когда слушаешь Apple Music</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3104"/>
