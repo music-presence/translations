fc4fcb2f0b016bf39427f8d67f3c9f5b50ca1788
moe <admin@moe.christmas>
2025-04-23 20:51:03 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (145 of 145 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/ko/

--- musicpresence_ko.ts
+++ musicpresence_ko.ts
@@ -600,34 +600,34 @@ You can support the development with a donation. Click here or in the tray menu 
         <location filename="../src/app/app.cpp" line="3300"/>
         <source>Tray icon theme</source>
         <comment>Setting for selecting the theme of the tray icon</comment>
-        <translation type="unfinished"></translation>
+        <translation>트레이 아이콘 스타일</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3309"/>
         <location filename="../src/app/app.cpp" line="3346"/>
         <source>System theme</source>
         <comment>Option to use the system theme for the app theme</comment>
-        <translation type="unfinished"></translation>
+        <translation>시스템 설정 테마</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3310"/>
         <location filename="../src/app/app.cpp" line="3347"/>
         <source>Light</source>
         <comment>Option to use a light app theme</comment>
-        <translation type="unfinished"></translation>
+        <translation>라이트</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3311"/>
         <location filename="../src/app/app.cpp" line="3348"/>
         <source>Dark</source>
         <comment>Option to use a dark app theme</comment>
-        <translation type="unfinished"></translation>
+        <translation>다크</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3337"/>
         <source>App theme</source>
         <comment>Setting for selecting the theme of the app</comment>
-        <translation type="unfinished"></translation>
+        <translation>앱 테마</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3452"/>
