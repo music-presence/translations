af8e27434f0512eb0a824f8025923688fba3f1fa
moe <admin@moe.christmas>
2025-04-23 20:55:55 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (145 of 145 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/nl/

--- musicpresence_nl.ts
+++ musicpresence_nl.ts
@@ -600,34 +600,34 @@ Je kan de ontwikkeling steunen met een donatie. Klik hier of in het taakbalkmenu
         <location filename="../src/app/app.cpp" line="3300"/>
         <source>Tray icon theme</source>
         <comment>Setting for selecting the theme of the tray icon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Taakbalk-pictogramthema</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3309"/>
         <location filename="../src/app/app.cpp" line="3346"/>
         <source>System theme</source>
         <comment>Option to use the system theme for the app theme</comment>
-        <translation type="unfinished"></translation>
+        <translation>Systeemthema</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3310"/>
         <location filename="../src/app/app.cpp" line="3347"/>
         <source>Light</source>
         <comment>Option to use a light app theme</comment>
-        <translation type="unfinished"></translation>
+        <translation>Licht</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3311"/>
         <location filename="../src/app/app.cpp" line="3348"/>
         <source>Dark</source>
         <comment>Option to use a dark app theme</comment>
-        <translation type="unfinished"></translation>
+        <translation>Donker</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3337"/>
         <source>App theme</source>
         <comment>Setting for selecting the theme of the app</comment>
-        <translation type="unfinished"></translation>
+        <translation>App-thema</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="3452"/>
