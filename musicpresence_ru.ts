578f4e18dbed694b2eb57de25750800b15d6d14e
Ayovu <ayovu@proton.me>
2025-06-06 06:59:50 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (159 of 159 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/ru/

--- musicpresence_ru.ts
+++ musicpresence_ru.ts
@@ -565,7 +565,7 @@ You can support the development with a donation. Click here or in the tray menu 
         <location filename="../src/app/app.cpp" line="3811"/>
         <source>Something is not working</source>
         <comment>help link: opens troubleshooting page</comment>
-        <translation type="unfinished"></translation>
+        <translation>Что-то не работает</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="4530"/>
@@ -855,7 +855,7 @@ You can support the development with a donation. Click here or in the tray menu 
         <location filename="../src/app/settings/window.cpp" line="328"/>
         <source>Advanced</source>
         <comment>settings category</comment>
-        <translation type="unfinished"></translation>
+        <translation>Продвинутые</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="332"/>
@@ -1218,25 +1218,25 @@ You can support the development with a donation. Click here or in the tray menu 
         <location filename="../src/app/settings/window.cpp" line="1366"/>
         <source>Media detection</source>
         <comment>advanced setting group: control how media is detected on the device</comment>
-        <translation type="unfinished"></translation>
+        <translation>Обнаружение медиа</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="1378"/>
         <source>Restart the app for these changes to take effect</source>
         <comment>media detection restart hint text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Перезагрузите приложение чтобы настройки заработали</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="1389"/>
         <source>Use native system interfaces</source>
         <comment>media detection: option to use native system APIs/interfaces</comment>
-        <translation type="unfinished"></translation>
+        <translation>Использовать нативные системные интерфейсы</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="1414"/>
         <source>Use AppleScript automation</source>
         <comment>media detection option</comment>
-        <translation type="unfinished"></translation>
+        <translation>Использовать автоматизацию AppleScript</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="1454"/>
