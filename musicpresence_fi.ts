ebf9b011d48246b91fa94ca93d08aa556fe64be2
ekimeister <ekimeister@noreply.codeberg.org>
2025-06-10 20:59:53 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (167 of 167 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/fi/

--- musicpresence_fi.ts
+++ musicpresence_fi.ts
@@ -469,13 +469,13 @@
         <location filename="../src/app/app.cpp" line="1505"/>
         <source>News from the developer</source>
         <comment>News tray menu title: This shows news about the app from the developer</comment>
-        <translation type="unfinished"></translation>
+        <translation>Uutiset kehittäjältä</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="1518"/>
         <source>Dismiss</source>
         <comment>News tray menu entry: Dismisses/ignores the news entry and removes it</comment>
-        <translation type="unfinished"></translation>
+        <translation>Hylkää</translation>
     </message>
     <message>
         <location filename="../src/app/app.cpp" line="4286"/>
@@ -848,19 +848,19 @@ Voit tukea kehitystä lahjoituksella. Paina tästä tai tehtäväpalkin kuvakkee
         <location filename="../src/app/news/news_dialog.cpp" line="57"/>
         <source>OK</source>
         <comment>Confirm dialog</comment>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../src/app/news/news_dialog.cpp" line="59"/>
         <source>Close anyway</source>
         <comment>Close a dialog anyways</comment>
-        <translation type="unfinished"></translation>
+        <translation>Sulje silti</translation>
     </message>
     <message>
         <location filename="../src/app/news/news_dialog.cpp" line="112"/>
         <source>News</source>
         <comment>News popup window title: This shows news about the app from the developer</comment>
-        <translation type="unfinished"></translation>
+        <translation>Uutiset</translation>
     </message>
 </context>
 <context>
@@ -916,7 +916,7 @@ Voit tukea kehitystä lahjoituksella. Paina tästä tai tehtäväpalkin kuvakkee
         <location filename="../src/app/settings/window.cpp" line="414"/>
         <source>Help with translating</source>
         <comment>opens the translation guide</comment>
-        <translation type="unfinished">Auta kääntämisessä</translation>
+        <translation>Auta kääntämisessä</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="419"/>
@@ -1021,13 +1021,13 @@ Voit tukea kehitystä lahjoituksella. Paina tästä tai tehtäväpalkin kuvakkee
         <location filename="../src/app/settings/window.cpp" line="745"/>
         <source>News</source>
         <comment>general setting group for app news</comment>
-        <translation type="unfinished"></translation>
+        <translation>Uutiset</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="749"/>
         <source>Notify about news from the developer</source>
         <comment>general setting: show a popup when there are news from the developer</comment>
-        <translation type="unfinished"></translation>
+        <translation>Ilmoita uutisista kehittäjältä</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="815"/>
