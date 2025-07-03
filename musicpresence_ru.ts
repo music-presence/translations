88f350c93207b6e56557a8f49140c4ad07be3a21
Ayovu <ayovu@proton.me>
2025-07-03 04:38:32 +0000

    Translated using Weblate (Russian)
    Currently translated at 99.4% (171 of 172 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/ru/

--- musicpresence_ru.ts
+++ musicpresence_ru.ts
@@ -1119,7 +1119,7 @@ You can support the development with a donation. Click here or in the tray menu 
         <location filename="../src/app/settings/window.cpp" line="1157"/>
         <source>Do not show any song information</source>
         <comment>appearance setting: whether to hide all song information</comment>
-        <translation type="unfinished"></translation>
+        <translation>Не показывать никакую ифнормацию о песне</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="1238"/>
