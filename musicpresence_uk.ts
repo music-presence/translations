9b3f297cb76e8fc832e39d1b8245c4e05c8a41f7
Ayovu <ayovu@proton.me>
2025-09-17 19:11:37 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (197 of 197 strings)
    
    Translation: Music Presence/Desktop Application
    Translate-URL: https://translate.codeberg.org/projects/music-presence/desktop-application/uk/

--- musicpresence_uk.ts
+++ musicpresence_uk.ts
@@ -1109,13 +1109,13 @@ You can support the development with a donation. Click here or in the tray menu 
         <location filename="../src/app/settings/window.cpp" line="2312"/>
         <source>Switch to AppleScript when an error occurs</source>
         <comment>services setting</comment>
-        <translation type="unfinished"></translation>
+        <translation>Переключатися на AppleScript, коли стається помилка</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="2315"/>
         <source>It can happen that the native system interface stops working with the next macOS update. Enable this option to automatically switch to AppleScript then.</source>
         <comment>media detection explanation: please use the same translations that were used for &quot;Use native system interfaces&quot; and &quot;Use AppleScript automation&quot;</comment>
-        <translation type="unfinished"></translation>
+        <translation>Може статися так що після наступного оновлення macOS нативний інтерфейс перестане працюват. Увімкніть цю опцію щоб автоматично переключитися до AppleScript в таком випадку.</translation>
     </message>
     <message>
         <location filename="../src/app/settings/window.cpp" line="2452"/>
